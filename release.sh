29e486bf1f0a70d7874ca149f26ea21c55dcb80d
Jared Friese <jared.friese@gmail.com>
2016-12-04 10:26:53 -0800

    Update release script

--- release.sh
+++ release.sh
@@ -2,17 +2,19 @@
 
 set -e
 
-ls
-ls /
 OUTPUT_DIR=$(pwd)/new-release
 echo $OUTPUT_DIR
 
 cd $GOPATH/src/github.com/jwfriese/omgfruitapi
 
+git config --global user.name "Jared Friese"
+git config --global user.email "jared.friese@gmail.com"
+
 go build main.go
 mv main $OUTPUT_DIR/release-binary
 
-NEXT=$(cat /proc/sys/kernel/random/uuid)
+CURRENT=$(git tag --sort="-refname" | head -1)
+NEXT="$(($CURRENT + 1))"
 
 echo "$NEXT" > $OUTPUT_DIR/name
 echo "$NEXT" > $OUTPUT_DIR/tag
