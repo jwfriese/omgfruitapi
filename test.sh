575f23c21bafb7d4fb60fbf66353adc67481b254
Jared Friese <jared.friese@gmail.com>
2016-12-04 07:07:21 -0800

    Add output to API test task

--- test.sh
+++ test.sh
@@ -10,3 +10,6 @@ go get github.com/onsi/gomega
 go get github.com/gorilla/mux
 
 ginkgo $GOPATH/src/github.com/jwfriese/omgfruitapi/fruit
+
+go build $GOPATH/src/github.com/jwfriese/omgfruitapi/main.go
+mv main new-release/runmeplz
